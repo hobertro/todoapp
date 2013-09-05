822a1e68e1f78019c9d47b9d64d99acea3673a5b
Robert Ho <boho527@gmail.com>
2013-09-05 00:50:49 -0700

    can add to the DOM now

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 // models
 
+$("#enter").on("click", function(){
+	var value = $("#inputTodo").val();
+	createView(value);
+});
+
+function createView(val){
+	var newView = new todoView({model: createModel(val)});
+	$("#todo").append(newView.render().el);
+}
+
+function createModel(val){
+	var todo = new Todo({title: val});
+	return todo;
+}
+
 var todo1 = new Todo();
 
 // views
 
 var todoview = new todoView({ model: todo1});
 
-console.log(todoview.render().el);
-
 $("#todo").append(todoview.render().el);
+
+// app view
+
+var appView = Backbone.View.extend({
+	initialize: {
+
+	}
+});
--- views/todoView.js
+++ views/todoView.js
@@ -1,5 +1,5 @@
 var todoView = Backbone.View.extend({
-
+	tagName: "li",
 	// will render each time a new view is created
 
 	template: Handlebars.compile($("#modelTemplate").html()),
@@ -10,6 +10,12 @@ var todoView = Backbone.View.extend({
 	render: function(){
 		this.$el.html(this.template(this.model.toJSON()));
 		return this;
+	},
+	events: {
+		"click ": "addModel"
+	},
+	addModel: function(){
+		this.render();
 	}
 });
 
